9b2929cf75c683713f8c9649a8ab5897967b8a0e
josonking <wangsanmu5@163.com>
2018-10-30 22:44:26 +0800

    update html redux files

--- 02-html-redux/src/html-redux.js
+++ 02-html-redux/src/html-redux.js
@@ -8,12 +8,36 @@ let createStore = (reducer) => {
 
   dispatch({});
 
-  let getState = () => {
-    JSON.parse(JSON.stringify(state))
-  }
+  let getState = () => JSON.parse(JSON.stringify(state));
 
   return {
     getState,
     dispatch,
+  };
+}
+
+let reducer = (state = initState, action) => {
+  switch (action.type) {
+    case 'add':
+      return {number: state.number + action.count}
+    default:
+      return state
   }
 }
+
+let store = createStore(reducer);
+function render() {
+  let content = document.querySelector('.content');
+  content.innerHTML = store.getState().number;
+}
+
+function add() {
+  store.dispatch({
+    type: 'add',
+    count: 2,
+  });
+  render();
+}
+
+render();
+
